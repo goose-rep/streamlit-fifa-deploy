8df76f3d3795c4a38259027f00ced373f9126652
goose-rep <gustavo.ttaa97@gmail.com>
2025-02-26 18:11:22 -0300

    Changed button to link_button

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -6,20 +6,10 @@ import data_load as dl
 
 dl.data_load()
 
-# if "data" not in st.session_state:
-#    df_data = pd.read_csv("datasets/CLEAN_FIFA23_official_data.csv", index_col=0)
-#    df_data = df_data[df_data["Contract Valid Until"] >= datetime.today().year]
-#    df_data = df_data[df_data["Value(£)"] > 0]
-#    df_data = df_data.sort_values(by="Overall", ascending=False)
-#    st.session_state["data"] = df_data
-
-
-
-
 st.markdown('# FIFA23 OFFICIAL DATASET ⚽')
 st.sidebar.markdown('Desenvolvido por Gustavo Alves')
 
-btn = st.button('Acesse os dados no Kaggle')
+btn = st.link_button('Acesse os dados no Kaggle')
 
 if btn:
    webbrowser.open_new_tab('https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data')
